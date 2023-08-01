7e968b0d65af14b2cc92bdf78ca9c356d05142a6
student <student@gnu.ac.in>
2023-08-01 19:11:18 +0530

    Initial commit

---
+++ app/src/main/java/com/rono/mad_practical_3_21012011103/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.rono.mad_practical_3_21012011103
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
